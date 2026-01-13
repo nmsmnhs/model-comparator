78b5a9c2c17cd21d57b4c4c0730203136f495487
nmsmnhs <phuongquynh.pham1202@gmail.com>
2026-01-14 00:09:14 +0700

    minor changes

--- backend.py
+++ backend.py
@@ -1,10 +1,17 @@
 from flask import Flask
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
-@app.route('/')
+@app.route('/', methods=["GET"])
 def fetch():
     ...
+
+@app.route('/results', methods=["POST"])
+def show_chart():
+    ...
+
 
 if __name__ == '__main__':
     app.run()
