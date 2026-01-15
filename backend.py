974feb1316e5a662b2eed7da7e6e34dfdc3f6549
nmsmnhs <phuongquynh.pham1202@gmail.com>
2026-01-15 23:17:00 +0700

    adding csv file upload

--- backend.py
+++ backend.py
@@ -1,12 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template
 from flask_cors import CORS
 
 app = Flask(__name__)
 CORS(app)
 
-@app.route('/', methods=["GET"])
-def fetch():
-    ...
+@app.route('/')
+def home():
+    return render_template('dropdown.html')
 
 @app.route('/results', methods=["POST"])
 def show_chart():
---
+++ static/script.js
@@ -0,0 +1,1 @@
+const datasetCsv = document.getElementById(csvFile)
