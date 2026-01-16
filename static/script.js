6528d2bf85f7eec210ca85ad78511ef2b59093ae
nmsmnhs <phuongquynh.pham1202@gmail.com>
2026-01-16 23:59:20 +0700

    minor changes

--- static/script.js
+++ static/script.js
@@ -1,1 +1,10 @@
-const datasetCsv = document.getElementById(csvFile)
+const form = document.querySelector("#csvFile")
+form.addEventListener("submit", async (event) => {
+  const formData = new FormData(form);
+
+  const response = await fetch("http://localhost:5000/", {
+    method: "POST",
+    body: formData,
+  });
+  event.preventDefault();
+});
