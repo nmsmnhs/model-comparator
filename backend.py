3bf1f443e21d77bd849efdd009e265634bcd5ec7
nmsmnhs <phuongquynh.pham1202@gmail.com>
2026-01-19 22:37:31 +0700

    updated load_csv

--- backend.py
+++ backend.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from flask_cors import CORS
 
 app = Flask(__name__)
@@ -7,6 +7,15 @@ CORS(app)
 @app.route('/')
 def home():
     return render_template('dropdown.html')
+
+@app.route('/load_csv', methods=["POST"])
+def get_csv():
+    if request.method == "POST":
+        f = request.files['csvFileInput']
+        if f.filename == '':
+            return f"No selected file"
+        else:
+            return f"{f} loaded"
 
 @app.route('/results', methods=["POST"])
 def show_chart():
--- static/script.js
+++ static/script.js
@@ -1,10 +1,11 @@
 const form = document.querySelector("#csvFile")
 form.addEventListener("submit", async (event) => {
+  event.preventDefault();
   const formData = new FormData(form);
 
-  const response = await fetch("http://localhost:5000/", {
+  const response = await fetch("http://127.0.0.1:5000/load_csv", {
     method: "POST",
     body: formData,
   });
-  event.preventDefault();
+  
 });
